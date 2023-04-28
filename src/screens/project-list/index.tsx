92f5006bd458160fe60c9123e0eb7206244580c1
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-04-28 16:01:17 +1000

    useDebounce useMount

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { List } from "screens/project-list/list";
 import { SearchPanel } from "screens/project-list/search-panel";
-import { cleanObject } from "utils";
+import { cleanObject, useMount, useDebounce } from "utils";
 import { stringify } from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL;
@@ -11,23 +11,25 @@ export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
   const [list, setList] = useState([]);
 
+  const debouncedParam = useDebounce(param, 2000);
+
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${stringify(cleanObject(param))}`).then(
+    fetch(`${apiUrl}/projects?${stringify(cleanObject(debouncedParam))}`).then(
       async (response) => {
         if (response.ok) {
           setList(await response.json());
         }
       }
     );
-  }, [param]);
+  }, [debouncedParam]);
 
-  useEffect(() => {
+  useMount(() =>
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
-    });
-  }, []);
+    })
+  );
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,23 @@
+import { useEffect, useState } from "react";
+
 export const cleanObject = (object) =>
   Object.fromEntries(Object.entries(object).filter(([key, value]) => value));
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const time = setTimeout(() => setDebouncedValue(value), delay);
+
+    //  clearTimeout will run every time when the dependencies change (useEffect re-run)
+    return () => clearTimeout(time);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
