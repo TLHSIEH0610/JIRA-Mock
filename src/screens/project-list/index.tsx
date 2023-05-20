3ab9b0f3ccbf123b342a9a6f1c0a1011c9cefb60
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-05-20 18:02:18 +1000

    add utils  useURLSearch

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,17 +2,17 @@ import { useState } from "react";
 import { List } from "screens/project-list/list";
 import { SearchPanel } from "screens/project-list/search-panel";
 import { useDebounce, useDocumentTitle } from "utils";
+import { useUrlQueryParam } from "utils/url";
 import styled from "@emotion/styled";
 import { useProjects } from "utils/project";
 import { useUsers } from "utils/user";
 import { Typography } from "antd";
 
 export const ProjectListScreen = () => {
-  const [param, setParam] = useState({ name: "", personId: "" });
+  const [param, setParam] = useUrlQueryParam(["name", "personId"]);
   const debouncedParam = useDebounce(param, 2000);
   const { isLoading, error, data: list } = useProjects(debouncedParam);
   const { data: users } = useUsers(debouncedParam);
-
   useDocumentTitle("Project List", false);
 
   return (
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,11 +1,23 @@
+import { useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
+import { cleanObject, subset } from "utils/index";
 
-export const useUrlQueryParam = (keys: string[]) => {
+export const useUrlQueryParam = <K extends string>(keys: K[]) => {
   const [searchParams, setSearchParam] = useSearchParams();
   return [
-    keys.reduce((prev, key) => {
-      return { ...prev, [key]: searchParams.get(key) };
-    }, {}),
-    setSearchParam,
-  ];
+    useMemo(
+      () =>
+        keys.reduce((prev, key) => {
+          return { ...prev, [key]: searchParams.get(key) || "" };
+        }, {} as { [key in K]: string }),
+      [searchParams]
+    ),
+    (params: Partial<{ [key in K]: unknown }>) => {
+      const o = cleanObject({
+        ...Object.fromEntries(searchParams),
+        ...params,
+      });
+      return setSearchParam(o);
+    },
+  ] as const;
 };
